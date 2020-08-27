d22f3dc9293ea33194d56f747d45035530cdd55b
Dimitrii <sexykdi@gmail.com>
2020-08-27 14:45:52 +0300

    mutext on ts/muxer and get_parameter for keep alive

--- format/rtsp/client.go
+++ format/rtsp/client.go
@@ -197,7 +197,7 @@ func (self *Client) SendRtpKeepalive() (err error) {
 				fmt.Println("rtp: keep alive")
 			}
 			req := Request{
-				Method: "OPTIONS",
+				Method: "GET_PARAMETER",
 				Uri:    self.requestUri,
 			}
 			if self.session != "" {
