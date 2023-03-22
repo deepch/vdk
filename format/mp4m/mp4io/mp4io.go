def88cb695aa222f7bc73dc7bb920c6963badd3c
deepch <admin@wetel.ru>
2023-03-22 22:01:39 +0300

    test transcode slow

--- format/mp4m/mp4io/mp4io.go
+++ format/mp4m/mp4io/mp4io.go
@@ -376,7 +376,7 @@ func (self *ElemStreamDesc) parseDescHdr(b []byte, offset int) (n int, tag uint8
 	return
 }
 
-func ReadFileAtoms(r io.ReadSeeker, name string) (atoms []Atom, err error) {
+func ReadFileAtoms(r io.ReadSeeker) (atoms []Atom, err error) {
 	for {
 		offset, _ := r.Seek(0, 1)
 		taghdr := make([]byte, 8)
